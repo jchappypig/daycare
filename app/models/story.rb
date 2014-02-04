73d68ee9a6f84a510424c2149d8bc01c1e2a868c
Huanhuan Huang <hhuang@thoughtworks.com>
2014-02-04 16:56:41 +1100

    display story photo when there is one when edit

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,4 @@
 class Story < ActiveRecord::Base
-  has_attached_file :photo, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  has_attached_file :photo, styles: { medium: '300x300>', thumb: '100x100>', large: '400x400>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
