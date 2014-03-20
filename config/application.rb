a67cf49f28b2b72a7203d1adad2e32d1c002acab
Huanhuan Huang <hhuang@thoughtworks.com>
2014-03-20 16:58:55 +1100

    updated dairies style.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Daycare
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
